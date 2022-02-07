628c59d59a3747f0ab57740b13ab1a6f4eaa249c
Kirill <dremkiri@gmail.com>
2022-02-07 23:53:14 +0200

    1

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// For 1
+// let sum
+// for(let i = 1; i <= 10; i++){
+//     sum = i ** 2
+//     console.log(sum)
+// }
+
+
+// For 2
+// let s = +prompt("enter s")
+// let sum = 0
+// for(let i = 1; i <= s; i++){
+//     sum = sum + i
+// }
+// console.log(sum)
+
+
+// For 3
+// let s = 10000
+// for(let i = 1; i <= 5; i++){
+//     s = s + (s * 0.1)
+// }
+// console.log(s)
+
+
+// For 4
+// for(let i = 1; i <= 10; i++){
+//     console.log('----------')
+//     console.log("          ")
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${j * i}`)
+//     }
+// }
